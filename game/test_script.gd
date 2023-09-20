0728d32f88ff7e71d157d16cdb6711b49fa3a7cb
Dimitri Lagast <lagast.dimitri@gmail.com>
2023-09-20 18:16:34 +0200

    Initial project

---
+++ game/test_script.gd
@@ -0,0 +1,10 @@
+extends TestNode2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super.say_hello("world")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ gdextension_mapgen/src/Node/2D/mappreview.h
@@ -0,0 +1,6 @@
+#ifndef NODE_2D_MAPPREVIEW
+#define NODE_2D_MAPPREVIEW
+
+
+
+#endif
