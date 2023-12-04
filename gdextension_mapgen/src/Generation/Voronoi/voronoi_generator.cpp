04bec318b68c27e8e28e33406789ccc8370644c4
Dimitri Lagast <lagast.dimitri@gmail.com>
2023-12-04 22:04:18 +0100

    split into functions

--- gdextension_mapgen/src/Generation/Voronoi/voronoi_generator.cpp
+++ gdextension_mapgen/src/Generation/Voronoi/voronoi_generator.cpp
@@ -30,6 +30,32 @@ namespace
 
         return rect;
     }
+
+    void fill_diagram_godot_cells(mapgen::voronoi::DiagramData& diagram)
+    {
+        for (const auto& cell : diagram.Cells)
+        {
+            diagram.GodotCellCenters.emplace_back(cell->Center);
+
+            std::vector<godot::Vector2> cell_points;
+            for (const auto& point : cell->Points)
+                cell_points.emplace_back(point->Position);
+            diagram.GodotCells.emplace_back(cell_points);
+        }
+    }
+
+    void fill_diagram_godot_edges(mapgen::voronoi::DiagramData& diagram)
+    {
+        for (const auto& edge : diagram.Edges)
+        {
+            godot::Vector4 edgeData;
+            edgeData.x = edge->Point1->Position.x;
+            edgeData.y = edge->Point1->Position.y;
+            edgeData.z = edge->Point2->Position.x;
+            edgeData.w = edge->Point2->Position.y;
+            diagram.GodotEdges.emplace_back(edgeData);
+        }
+    }
 } // namespace
 
 mapgen::voronoi::DiagramData mapgen::voronoi::generation::generate(const std::vector<godot::Vector2>& points, int width, int height)
@@ -44,26 +70,8 @@ mapgen::voronoi::DiagramData mapgen::voronoi::generation::generate(const std::ve
     jcv_diagram_generate(jcv_points.size(), jcv_points.data(), &rect, 0, &jcv_diagram);
 
     auto diagram = converter::extract_data(jcv_diagram);
-
-    // ToDo: separate
-    for(const auto& cell : diagram.Cells)
-    {
-        diagram.GodotCellCenters.emplace_back(cell->Center);
-
-        std::vector<godot::Vector2> cell_points;
-        for (const auto& point : cell->Points)
-            cell_points.emplace_back(point->Position);
-        diagram.GodotCells.emplace_back(cell_points);
-    }
-    for(const auto& edge : diagram.Edges)
-    {
-        godot::Vector4 edgeData;
-        edgeData.x = edge->Point1->Position.x;
-        edgeData.y = edge->Point1->Position.y;
-        edgeData.z = edge->Point2->Position.x;
-        edgeData.w = edge->Point2->Position.y;
-        diagram.GodotEdges.emplace_back(edgeData);
-    }
+    fill_diagram_godot_cells(diagram);
+    fill_diagram_godot_edges(diagram);
 
     jcv_diagram_free(&jcv_diagram);
 
